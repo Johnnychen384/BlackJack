c5f8f0132d6145924c3ac16758f5ec371dfe2c32
Johnny Chen <Johnnychen384@gmail.com>
2022-04-10 20:16:53 -0400

    initial commit

---
+++ BlackJack.js
@@ -0,0 +1,78 @@
+let moneyWon;
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message;
+let messageEl = document.getElementById('message-el')
+let sumEl = document.querySelector('#sum-el')
+let cardsEl = document.querySelector('#cards-el')
+
+let player = {
+    name : "Per",
+    chips : 200
+}
+
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name+ ": $" + player.chips
+
+
+
+function getRandomCard(){
+    randomNumber = Math.floor(Math.random()*13) + 1
+    if (randomNumber === 1){
+        return 11
+    } else if (randomNumber === 11) {
+        return 10
+    } else if (randomNumber === 12) {
+        return 10
+    } else if (randomNumber === 13) {
+        return 10
+    } else {
+        return randomNumber
+    }
+}
+
+
+function startGame(){
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards.push(firstCard)
+    cards.push(secondCard)
+    sum += firstCard + secondCard
+    renderGame()
+}
+
+
+function renderGame(){
+    if (sum <= 20){
+        message = 'Do you want to draw a new card?'
+    } else if (sum === 21) {
+        message = "Wohoo! You've got BlackJack!"
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game! T_T "
+        isAlive = false;
+    }
+    messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " 
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+}
+
+
+
+function newCard(){
+    if (isAlive === true && hasBlackJack === false){
+        let thirdCard = getRandomCard()
+        sum += thirdCard
+        cards.push(thirdCard)
+        renderGame()
+    }
+}
+
+
